e201092630b2015b277756d73088b134e935f981
kanomdook <kanung.2535@gmail.com>
2017-11-24 10:13:31 +0700

    add shop

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,15 +3,18 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ShopPage } from '../shop/shop';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  tab1Root = HomePage;
+  
+  tab1Root = ShopPage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = ContactPage;
+  tab5Root = ContactPage;
 
   constructor() {
 
