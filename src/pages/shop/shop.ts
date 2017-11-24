e0b7c53690fd982ff8f096a9882b1c8eba1864d6
abhisit snk <abhisit2537@hotmail.com>
2017-11-24 11:55:53 +0700

    shop mobile

--- src/pages/shop/shop.ts
+++ src/pages/shop/shop.ts
@@ -7,22 +7,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'shop.html',
 })
 export class ShopPage {
-  private imgs: Array<any> = [];
+  private cate: Array<any> = [];
+  private product: Array<any> = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    this.imgs = [
-      'https://images.unsplash.com/photo-1485963631004-f2f00b1d6606?auto=format&fit=crop&w=668&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-      'https://images.unsplash.com/photo-1485963631004-f2f00b1d6606?auto=format&fit=crop&w=668&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-      'https://images.unsplash.com/photo-1453831362806-3d5577f014a4?auto=format&fit=crop&w=738&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-      'https://images.unsplash.com/photo-1485963631004-f2f00b1d6606?auto=format&fit=crop&w=668&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-      'https://images.unsplash.com/photo-1485963631004-f2f00b1d6606?auto=format&fit=crop&w=668&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-      'https://images.unsplash.com/photo-1485963631004-f2f00b1d6606?auto=format&fit=crop&w=668&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-      'https://images.unsplash.com/photo-1485963631004-f2f00b1d6606?auto=format&fit=crop&w=668&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
+    this.cate = [
+      'http://blankrangemusic.com/images/camera.svg',
+      'http://blankrangemusic.com/images/camera.svg',
+      'http://blankrangemusic.com/images/camera.svg',
+      'http://blankrangemusic.com/images/camera.svg',
+      'http://blankrangemusic.com/images/camera.svg',
+      'http://blankrangemusic.com/images/camera.svg',
     ];
-
+    this.product = [
+      'http://blankrangemusic.com/images/camera.svg',
+      'http://blankrangemusic.com/images/camera.svg',
+      'http://blankrangemusic.com/images/camera.svg',
+    ];
   }
+  
 
 }
